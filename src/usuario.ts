6ca8b53bf7b150e8ca72769c5475f685840d47cc
NannFernandez <nanfernandezok@gmail.com>
2020-09-07 01:43:39 -0300

    class usuario

---
+++ src/usuario.ts
@@ -0,0 +1,33 @@
+import { CondicionAlimenticia } from "./condicionAlimenticia"
+
+export class Usuario {
+
+    constructor(
+        public nombreYApellido: string,
+        public peso: number,
+        public estatura: number,
+        public condicionesAlimenticias: CondicionAlimenticia[] = []) { }
+
+    indiceMasaCorporal() {
+        return this.peso / Math.pow(this.estatura, 2)
+    }
+
+    agregarCondicionAlimenticia(condicion: CondicionAlimenticia) {
+        this.condicionesAlimenticias.push(condicion)
+    }
+
+    imcEsSaludable() {
+        return this.indiceMasaCorporal() > 18 && this.indiceMasaCorporal() < 30
+    }
+
+    esSaludable() {
+        return this.imcEsSaludable() && (this.condicionesAlimenticias.length > 0 || this.subsanaCondicionesPreexistentes())
+    }
+
+    subsanaCondicionesPreexistentes() {
+        return this.condicionesAlimenticias.every(condicionAlimenticia => condicionAlimenticia.subsanaCondicion(this))
+    }
+
+}
+
+
