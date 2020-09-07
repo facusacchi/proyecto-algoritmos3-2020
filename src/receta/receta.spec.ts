748df2f74537ea30a93210a9643950a6163d2010
Facu <faq.ndo.n.s@gmail.com>
2020-09-07 11:24:41 -0300

    agrego test a receta

--- src/receta/receta.spec.ts
+++ src/receta/receta.spec.ts
@@ -4,8 +4,8 @@ import { Alimento } from '../alimento'
 import { CondicionAlimenticia, vegetariano, vegano } from '../condicionAlimenticia'
 
 let receta: Receta
-let usuario: Usuario
-let alimento: Alimento
+let usuarioVegetariano: Usuario
+let alimentoParaNoVegetarianos: Alimento
 let condiciones: Array<CondicionAlimenticia> = []
 let alimentosGustados: Array<Alimento> = []
 
@@ -13,24 +13,28 @@ describe('dada una receta...', () => {
     
     beforeEach(() => {
         condiciones.push(vegetariano)
-        alimentosGustados.push(alimento)
-        receta = new Receta(usuario)
-        alimento = new Alimento('papaya', 'dorada', 'HORTALIZAS_FRUTAS_SEMILLAS', condiciones)
-        usuario = new Usuario('lolo', 80, 175, new Date(), condiciones, alimentosGustados, 'LEVE')
+        alimentosGustados.push(alimentoParaNoVegetarianos)
+        receta = new Receta(usuarioVegetariano)
+        alimentoParaNoVegetarianos = new Alimento('papaya', 'dorada', 'HORTALIZAS_FRUTAS_SEMILLAS', condiciones)
+        usuarioVegetariano = new Usuario('lolo', 80, 175, new Date(), condiciones, alimentosGustados, 'LEVE')
     })
 
     test('puede ser editada por un autor', () => {
-        receta.agregarAutor(usuario)
-        expect(true).toBe(receta.esEditablePor(usuario))
+        receta.agregarAutor(usuarioVegetariano)
+        expect(true).toBe(receta.esEditablePor(usuarioVegetariano))
     }) 
     
     test('puede ser editada por un colaborador', () => {
-        receta.agregarColaborador(usuario)
-        expect(true).toBe(receta.esEditablePor(usuario))
+        receta.agregarColaborador(usuarioVegetariano)
+        expect(true).toBe(receta.esEditablePor(usuarioVegetariano))
     })
 
     test('un usuario que no es autor ni colaborador, no puede editarla', () => {
-        expect(false).toBe(receta.esEditablePor(usuario))
+        expect(false).toBe(receta.esEditablePor(usuarioVegetariano))
+    })
+
+    test('es inadecuada para un usuario que contenga las condiciones inadecuadas de la receta', () => {
+        expect(vegetariano).toBe(receta.condicionesInadecuadasReceta)  
     })
 
 })
