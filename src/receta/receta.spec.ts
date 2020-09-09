66b443343090496e9484854a6c10036ba4027659
Facu <faq.ndo.n.s@gmail.com>
2020-09-09 19:57:11 -0300

    refactor clase receta

--- src/receta/receta.spec.ts
+++ src/receta/receta.spec.ts
@@ -6,35 +6,35 @@ import { CondicionAlimenticia, vegetariano } from '../condicionAlimenticia'
 let receta: Receta
 let usuarioVegetariano: Usuario
 let alimentoParaNoVegetarianos: Alimento
-let condiciones: Array<CondicionAlimenticia> = []
-let alimentosGustados: Array<Alimento> = []
+const condiciones: Array<CondicionAlimenticia> = []
+const alimentosGustados: Array<Alimento> = []
 
 describe('dada una receta...', () => {
     
-    beforeEach(() => {
-        condiciones.push(vegetariano)
-        alimentosGustados.push(alimentoParaNoVegetarianos)
-        receta = new Receta(usuarioVegetariano)
-        alimentoParaNoVegetarianos = new Alimento('papaya', 'dorada', 'HORTALIZAS_FRUTAS_SEMILLAS', condiciones)
-        usuarioVegetariano = new Usuario('lolo', 80, 175,  condiciones, new Date, alimentosGustados, 'LEVE')
-    })
+  beforeEach(() => {
+    condiciones.push(vegetariano)
+    alimentosGustados.push(alimentoParaNoVegetarianos)
+    receta = new Receta(usuarioVegetariano)
+    alimentoParaNoVegetarianos = new Alimento('papaya', 'dorada', 'HORTALIZAS_FRUTAS_SEMILLAS', condiciones)
+    usuarioVegetariano = new Usuario('lolo', 80, 175,  condiciones, new Date, alimentosGustados, 'LEVE')
+  })
 
-    test('puede ser editada por un autor', () => {
-        receta.agregarAutor(usuarioVegetariano)
-        expect(true).toBe(receta.esEditablePor(usuarioVegetariano))
-    }) 
+  test('puede ser editada por un autor', () => {
+    receta.agregarAutor(usuarioVegetariano)
+    expect(true).toBe(receta.esEditablePor(usuarioVegetariano))
+  }) 
     
-    test('puede ser editada por un colaborador', () => {
-        receta.agregarColaborador(usuarioVegetariano)
-        expect(true).toBe(receta.esEditablePor(usuarioVegetariano))
-    })
+  test('puede ser editada por un colaborador', () => {
+    receta.agregarColaborador(usuarioVegetariano)
+    expect(true).toBe(receta.esEditablePor(usuarioVegetariano))
+  })
 
-    test('un usuario que no es autor ni colaborador, no puede editarla', () => {
-        expect(false).toBe(receta.esEditablePor(usuarioVegetariano))
-    })
+  test('un usuario que no es autor ni colaborador, no puede editarla', () => {
+    expect(false).toBe(receta.esEditablePor(usuarioVegetariano))
+  })
 
-    test('es inadecuada para un usuario que contenga las condiciones inadecuadas de la receta', () => {
-        expect(vegetariano).toBe(receta.condicionesInadecuadasReceta)  
-    })
+  test('es inadecuada para un usuario que contenga las condiciones inadecuadas de la receta', () => {
+    expect(vegetariano).toBe(receta.condicionesInadecuadasReceta)  
+  })
 
 })
--- src/receta/receta.ts
+++ src/receta/receta.ts
@@ -1,35 +1,34 @@
-import { CondicionAlimenticia } from "../condicionAlimenticia";
-import { Ingrediente } from "../ingrediente";
-import {Usuario} from "../usuario";
-import { flatMap } from "lodash";
+import { CondicionAlimenticia } from '../condicionAlimenticia'
+import { Ingrediente } from '../ingrediente'
+import { Usuario } from '../usuario'
 
 export class Receta {
 
-    constructor(public autor: Usuario) {}
+  constructor(public autor: Usuario) {}
     public colaboradores: Array<Usuario> = []
     public ingredientes: Array<Ingrediente> = []
 
-    esEditablePor(usuario: Usuario): Boolean {
+    esEditablePor(usuario: Usuario): boolean {
       return usuario === this.autor || this.colaboradores.includes(usuario)
     }
     
-    agregarColaborador(colaborador: Usuario) {
-		  this.colaboradores.push(colaborador)
+    agregarColaborador(colaborador: Usuario): void {
+      this.colaboradores.push(colaborador)
     }
 
-    agregarAutor(autor: Usuario) {
+    setearAutor(autor: Usuario): void {
       this.autor = autor
     }
     
-    agregarIngrediente(ingrediente: Ingrediente) {
-		  this.ingredientes.push(ingrediente)
+    agregarIngrediente(ingrediente: Ingrediente): void {
+      this.ingredientes.push(ingrediente)
     }
     
-    condicionesInadecuadasReceta() {
-      return (this.ingredientes.flatMap( ingrediente => { ingrediente.condicionesInadecuadasIngrediente() } ))
+    condicionesInadecuadasReceta(): CondicionAlimenticia[] {
+      return (this.ingredientes.flatMap( ingrediente => ingrediente.condicionesInadecuadasIngrediente()))
     }    
 
-    getIngredientes() {
+    getIngredientes(): Ingrediente[] {
       return this.ingredientes
     }
 }
@@ -37,18 +36,18 @@ export class Receta {
 export class RecetaCompuesta extends Receta{
   public subrecetas: Array<Receta> = []
 
-  agregarSubreceta(subreceta: Receta) {
+  agregarSubreceta(subreceta: Receta): void {
     this.subrecetas.push(subreceta)
   }
 
-  condicionesInadecuadasReceta() {
-    return this.getIngredientes().flatMap(ingrediente => { ingrediente.condicionesInadecuadasIngrediente() })
+  condicionesInadecuadasReceta(): CondicionAlimenticia[] {
+    return this.getIngredientes().flatMap(ingrediente =>  ingrediente.condicionesInadecuadasIngrediente())
   }
 
-  getIngredientes() {
-    var _ingredientes = []
-    _ingredientes.push(this.ingredientes)
-    _ingredientes.push(this.subrecetas.flatMap(receta => { receta.getIngredientes() }))
+  getIngredientes(): Ingrediente[] {
+    let _ingredientes: Ingrediente[] = []
+    _ingredientes = _ingredientes.concat(this.ingredientes)
+    _ingredientes = _ingredientes.concat((this.subrecetas.flatMap(receta => receta.getIngredientes())))
     return _ingredientes
   }
 }
