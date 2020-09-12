c4733c6de1a0149b0f481bfe89f025b4a8ca89b1
Facu <faq.ndo.n.s@gmail.com>
2020-09-12 01:06:06 -0300

    agrego test de receta compuesta

--- src/receta.spec.ts
+++ src/receta.spec.ts
@@ -1,24 +1,27 @@
-import { Receta } from './Receta'
-import { Usuario, Rutina } from './usuario'
+import { Receta, RecetaCompuesta } from './Receta'
+import { Usuario } from './usuario'
 import { Alimento } from './alimento'
-import { CondicionAlimenticia, vegetariano } from './condicionAlimenticia'
+import { vegetariano, vegano } from './condicionAlimenticia'
 import { Ingrediente } from './ingrediente'
 
 let receta: Receta
+let recetaCompueta: RecetaCompuesta
 let usuarioVegetariano: Usuario
 let alimentoParaNoVegetarianos: Alimento
+let alimentoParaNoVeganos: Alimento
 let ingredienteParaNoVegetarianos: Ingrediente
-const alimentosGustados: Alimento[] = []
+let ingredienteParaNoVeganos: Ingrediente
 
 describe('dada una receta...', () => {
     
   beforeEach(() => {
-    alimentosGustados.push(alimentoParaNoVegetarianos)
     receta = new Receta(usuarioVegetariano)
-    ingredienteParaNoVegetarianos = new Ingrediente(alimentoParaNoVegetarianos,'125')
-    receta.agregarIngrediente(ingredienteParaNoVegetarianos)
+    recetaCompueta = new RecetaCompuesta(usuarioVegetariano)
     alimentoParaNoVegetarianos = new Alimento('papaya', 'dorada', 'HORTALIZAS_FRUTAS_SEMILLAS', [vegetariano])
-    usuarioVegetariano = new Usuario('lolo', 80, 175,  [vegetariano], new Date, alimentosGustados, 'LEVE')
+    alimentoParaNoVeganos = new Alimento('Vegano', 'No', 'HORTALIZAS_FRUTAS_SEMILLAS', [vegano])
+    ingredienteParaNoVegetarianos = new Ingrediente(alimentoParaNoVegetarianos, '125')
+    ingredienteParaNoVeganos = new Ingrediente(alimentoParaNoVeganos, '200')
+    usuarioVegetariano = new Usuario('lolo', 80, 175,  [vegetariano], new Date, [alimentoParaNoVegetarianos], 'LEVE')
   })
 
   test('puede ser editada por un autor', () => {
@@ -36,7 +39,15 @@ describe('dada una receta...', () => {
   })
 
   test('si le pregunto por sus condiciones inadecuadas, me devuelve los valores que coincidan con dichas condiciones', () => {
+    receta.agregarIngrediente(ingredienteParaNoVegetarianos)
     expect(receta.condicionesInadecuadasReceta()).toStrictEqual([vegetariano])
+  })
+
+  test('al ser compuesta y tener una subreceta, sus condiciones inadecuadas seran las suyas mas las de su subreceta', () => {
+    receta.agregarIngrediente(ingredienteParaNoVegetarianos)
+    recetaCompueta.agregarIngrediente(ingredienteParaNoVeganos)
+    recetaCompueta.agregarSubreceta(receta)
+    expect(recetaCompueta.condicionesInadecuadasReceta()).toStrictEqual([vegano, vegetariano])
   })
 
 })
