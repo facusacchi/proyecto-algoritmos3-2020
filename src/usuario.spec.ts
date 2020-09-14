d58bc74509b4177733dd67d219d58b734effe2af
NannFernandez <42684465+NannFernandez@users.noreply.github.com>
2020-09-13 21:06:22 -0300

    invierto tobe y expect en test de usuario

--- src/usuario.spec.ts
+++ src/usuario.spec.ts
@@ -5,24 +5,24 @@ describe('un usuario', () => {
     
     test('con imc saludable y sin condiciones alimenticias es saludable', () => {
        const usuario = new Usuario('pepe', 80 , 1.70)
-       expect(true).toBe(usuario.esSaludable())
+       expect(usuario.esSaludable()).toBe(true)
     })
     
     test('con imc saludable, con condiciones alimenticias no subsanadas no es saludable ', () => {
         const usuario = new Usuario('pepe', 80 , 1.70, [hipertenso])
         usuario.rutina = 'LEVE'
-        expect(false).toBe(usuario.esSaludable())
+        expect(usuario.esSaludable()).toBe(false)
     })
 
     test('con imc saludable, con condiciones alimenticias subsanadas es saludable ', () => {
         const usuario = new Usuario('pepe', 80 , 1.70, [hipertenso])
         usuario.rutina = "INTENSIVO"
-        expect(true).toBe(usuario.esSaludable())
+        expect(usuario.esSaludable()).toBe(true)
     })
 
     test('con imc no saludable no es saludable ', () => {
         const usuario = new Usuario('pepe', 120 , 1.50)
-        expect(false).toBe(usuario.esSaludable())
+        expect(usuario.esSaludable()).toBe(false)
     })
 
 
