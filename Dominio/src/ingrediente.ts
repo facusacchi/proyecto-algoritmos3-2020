46d97b6d2f4b1fcbd99d6dbbb7f49f7d2bf81b15
Facu <faq.ndo.n.s@gmail.com>
2020-09-15 18:40:45 -0300

    borro directorio dominio para mergear con devTypescript

--- Dominio/src/ingrediente.ts
+++
@@ -1,9 +0,0 @@
-import { Alimento } from "./alimento"
-
-export class Ingrediente {
-	constructor(public alimento: Alimento, public cantidad: string) { }
-
-	public condicionesInadecuadasIngrediente() {
-		return this.alimento.condicionesInadecuadas
-	}
-}
