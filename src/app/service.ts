2706b9498d4524428e4e0786dc93ef37d13ae7d6
Facu <faq.ndo.n.s@gmail.com>
2020-09-25 23:01:31 -0300

    creo service

---
+++ src/app/service.ts
@@ -0,0 +1,29 @@
+import { Usuario } from '../../Dominio/src/usuario'
+import { Alimento } from '../../Dominio/src/alimento'
+import { vegetariano, vegano, hipertenso } from '../../Dominio/src/condicionAlimenticia'
+import { Injectable } from '@angular/core'
+
+@Injectable({
+    providedIn: 'root',
+  })
+
+export class Service {
+    private usuarios: Usuario[] = []
+    private alimentos: Alimento[] = []
+    papa: Alimento
+    carneVacuna: Alimento
+
+    constructor() {
+        this.papa = new Alimento('Papa', '---', 'HORTALIZAS_FRUTAS_SEMILLAS', [hipertenso])
+        this.carneVacuna = new Alimento('Carne Vacuna', '---', 'CARNES_PESCADO_HUEVO', [vegetariano, vegano])
+        this.usuarios = [
+            new Usuario("Pepe Palala", 95, 1.75, [vegetariano], new Date(1991, 1, 28), [this.papa], 'NADA'),
+            new Usuario("Juan Carlos De La Hoya", 120, 1.90, [vegano], new Date(1985, 5, 7), [this.carneVacuna], 'MEDIANO'),
+            new Usuario("Manolo Palala", 80, 1.60, [hipertenso], new Date(1988, 7, 14), [this.carneVacuna], 'INTENSIVO')
+        ]
+    }
+
+    buscarUsuarios() {
+        return this.usuarios
+    }
+}
