f7604d1bacb4765f4fb1e4712550dc0d1b26cbc4
Facu <faq.ndo.n.s@gmail.com>
2020-09-29 23:40:25 -0300

    arreglo error en componente condiciones

--- src/app/components/perfil-usuario-form/perfil-usuario-form.component.ts
+++ src/app/components/perfil-usuario-form/perfil-usuario-form.component.ts
@@ -13,18 +13,26 @@ export class PerfilUsuarioFormComponent implements OnInit {
   usuario: Usuario
   opcionesRutina: Rutina[] = ["NADA", "LEVE", "MEDIANO", "ACTIVO", "INTENSIVO"]
   opcionElegida: Rutina
+  fecha: String = "1991-01-28"
 
   constructor(private route: ActivatedRoute) {
     this.usuario = service.getUsuarioLogueado()
   }
   
+  
+  ngOnInit(): void {
+    this.formatearFecha()
+  }
+  
+  formatearFecha(){
+    const fecha = this.usuario.fechaDeNacimiento.toISOString()
+    console.log(fecha.substring(0, 10))
+  }
+
   getStatus(): String {
     if(this.usuario.imcEsSaludable()) {
       return "Estado Saludable"
     } else {return "No Saludable"}
-  }
-  
-  ngOnInit(): void {
   }
 
 
