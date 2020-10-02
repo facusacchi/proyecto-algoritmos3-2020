3206749e0c1cb66b4178061985b751e8a1ece450
Facu <faq.ndo.n.s@gmail.com>
2020-10-02 09:13:59 -0300

    elimino todo el codigo comentado del html y el ts de perfil

--- src/app/components/perfil-usuario-form/perfil-usuario-form.component.ts
+++ src/app/components/perfil-usuario-form/perfil-usuario-form.component.ts
@@ -2,36 +2,26 @@ import { Component, OnInit } from '@angular/core';
 import { Usuario, Rutina } from '../../../../Dominio/src/usuario';
 import { service } from '../../service';
 import { ActivatedRoute } from '@angular/router';
-import { Alimento } from '../../../../Dominio/src/alimento';
 
 @Component({
   selector: 'app-perfil-usuario-form',
   templateUrl: './perfil-usuario-form.component.html',
   styleUrls: ['./perfil-usuario-form.component.css']
 })
+
 export class PerfilUsuarioFormComponent implements OnInit {
   
   usuario: Usuario
   opcionesRutina: Rutina[] = ["NADA", "LEVE", "MEDIANO", "ACTIVO", "INTENSIVO"]
   opcionElegida: Rutina
   fecha: String = "2020-01-12"
-  /* alimentosPreferidos: Alimento[]
-  alimentosPreferidosParseados: String[] */
   alimentosPreferidos: String[]
   constructor(private route: ActivatedRoute) {
     this.usuario = service.getUsuarioLogueado
-    /* this.alimentosPreferidos = service.getUsuarioLogueado.alimentosPreferidos */
     this.alimentosPreferidos = service.parsearAlimentosAString(service.getUsuarioLogueado.alimentosPreferidos)
   }
   
-  ngOnInit(): void {
-  }
-
-/*   parsearAlimentosAString(): String[]{
-    const alimentos: String[] = []
-    this.alimentosPreferidos.forEach(alimento => alimentos.push(alimento.nombre))
-    return alimentos
-  } */
+  ngOnInit(): void { }
 
   getStatus(): String {
     if(this.usuario.imcEsSaludable()) {
