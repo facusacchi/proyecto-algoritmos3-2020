5ce2591487e8941e195c33b2cfa87e0e2dcc1bca
NannFernandez <nanfernandezok@gmail.com>
2020-10-04 20:25:45 -0300

    funcionalidad de agregar ingredientes

--- src/app/components/agregar-ingrediente/agregar-ingrediente.component.ts
+++ src/app/components/agregar-ingrediente/agregar-ingrediente.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Service } from 'app/service';
 import { Alimento } from '../../../../Dominio/src/alimento';
+import { Ingrediente } from '../../../../Dominio/src/ingrediente';
 import { Receta } from '../../../../Dominio/src/receta';
 
 @Component({
@@ -13,6 +14,9 @@ export class AgregarIngredienteComponent implements OnInit {
 
   receta : Receta
   alimentos : Alimento[]
+  alimentoSeleccionado  : Alimento 
+  cantidad : string
+  
 
   constructor(private route: ActivatedRoute, private service : Service) {
     this.route.params.subscribe((agregarIngredienteParameters) => {
@@ -22,6 +26,23 @@ export class AgregarIngredienteComponent implements OnInit {
 
   ngOnInit() {
     this.alimentos=this.service.getAlimentos()
+  }
+
+  seleccionarAlimento(alimento : Alimento) {
+    this.alimentoSeleccionado = alimento
+    console.log(alimento.nombre)
+  }
+ 
+  agregarIngrediente() {
+    let ingredienteNuevo : Ingrediente = new Ingrediente(this.alimentoSeleccionado, this.cantidad)
+    this.receta.agregarIngrediente(ingredienteNuevo)
+  }
+
+  colorSeleccionado(alimento : Alimento) {
+    if( this.alimentoSeleccionado == alimento){
+      return "colorSeleccionado"
+    }
+    return ""
   }
 
 }
