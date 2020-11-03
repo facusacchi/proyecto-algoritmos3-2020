c14197aee4d36b86d18d48d950af41826dbeaddb
NannFernandez <nanfernandezok@gmail.com>
2020-11-03 18:42:05 -0300

    agrego un button

--- React/src/login/login.js
+++ React/src/login/login.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Card } from "primereact/card";
 import "./login.css"
 import { Button } from 'primereact/button';
+import { InputText } from 'primereact/inputtext';
 
 export class LoginComponent extends Component {
   constructor(props) {
@@ -17,6 +18,12 @@ export class LoginComponent extends Component {
       <div className = "centrado">
         <Card className = "cardLogin">
         <div className="titulo">TeleFood</div>
+        <InputText value={this.state.value} onChange={(e) => this.setState({value: e.target.value})} />
+        <span className="p-float-label">
+    <InputText id="in" value={this.state.value} onChange={(e) => this.setState({value: e.target.value})} />
+    <label htmlhtmlFor="in">Username</label>
+</span>
+
         <Button label="Save" />
         </Card>
       </div>
