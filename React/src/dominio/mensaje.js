446861c7be2be593ae3ea7f4cd5a06d09fe7f387
NannFernandez <nanfernandezok@gmail.com>
2020-11-08 00:30:43 -0300

    component inbox con tabla y busqueda

---
+++ React/src/dominio/mensaje.js
@@ -0,0 +1,10 @@
+export default class Mensaje {
+
+    constructor(fecha , emisor, leido = false) {
+        this.fecha = fecha
+        this.emisor = emisor
+        this.leido = leido
+      }
+    
+
+}
--- React/src/inbox/inbox.js
+++ React/src/inbox/inbox.js
@@ -4,6 +4,7 @@ import "./inbox.css"
 import { Button } from 'primereact/button';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
+import Mensaje from "../dominio/mensaje";
 
 export class InboxComponent extends Component {
   constructor(props) {
@@ -13,6 +14,45 @@ export class InboxComponent extends Component {
     };
   }
 
+  async componentDidMount () {
+    const mensajes = [
+      new Mensaje("10/11/2020", "Juan"),
+      new Mensaje("03/04/2020", "Carlos"),
+      new Mensaje("09/08/2020", "Paula", true),
+      new Mensaje("09/08/2020", "Mariana")
+    ]
+    this.setState({mensajes : mensajes}) 
+  }
+
+  leidoTemplate = ({leido}) => {
+    return (
+      leido ?
+      //<span title="No leído"  style={{ display: 'flex', alignItems: 'center', alignContent: 'center', justifyContent: 'center' }}>
+      <i className="pi pi-inbox"></i>
+      //</span>
+    :
+      //<span title="No leído"  style={{ display: 'flex', alignItems: 'center', alignContent: 'center', justifyContent: 'center' }}>
+      <i className="pi pi-envelope"></i>
+      // </span>
+    )
+  }
+
+  leerTemplate = ({leido}) => {
+    return (
+      leido ?
+          <i className="pi pi-eye-slash border"></i> :
+        //<span title="No leído" data-testid={'noLeido' + id} className="p-badge p-badge-warning icon-badge" style={{ display: 'flex', alignItems: 'center', alignContent: 'center', justifyContent: 'center' }}>
+          <i className="pi pi-eye border"></i>
+        //</span>
+    )
+  }
+
+  eliminarTemplate = (mensaje) => {
+    return (
+          <i className="pi pi-trash border"></i>
+    )
+  }
+ 
   render() {
     return (
       <div className = "separacion">
@@ -21,13 +61,12 @@ export class InboxComponent extends Component {
         <Button icon="pi pi-search iconoBusqueda" iconPos="right" />
         <h2>Resultados de la búsqueda</h2>
         <DataTable value={this.state.mensajes}>
-                <Column field="code" ></Column>
-                <Column field="date" ></Column>
-                <Column field="category" ></Column>
-                <Column field="quantity" ></Column>
-                <Column field="quantity" ></Column>
+                <Column body={this.leidoTemplate} ></Column>
+                <Column field="fecha" ></Column>
+                <Column field="emisor" ></Column>
+                <Column body={this.leerTemplate} ></Column>
+                <Column body={this.eliminarTemplate}></Column>
             </DataTable>
-
       </div>
     );
   } 
