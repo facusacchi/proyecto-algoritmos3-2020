a68a319c69e2ce9ee6f9ba38f169393fe77bd581
Mauricio Carunchio <mcarunchio@baufest.com>
2020-11-10 21:07:23 -0300

    Saco imports que no se usan

--- React/src/App.js
+++ React/src/App.js
@@ -3,8 +3,6 @@ import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 import { FooterComponent } from './footer/footer';
-import { Header } from './header/header'
-import { MenuHeader } from './menu-header/menu-header'
 import { MensajesRoutes } from './route.js'
 import 'primeflex/primeflex.css'
 
