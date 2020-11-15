ed91c56fe2cc2445d1ef2a2f7fee517df10919a3
NannFernandez <nanfernandezok@gmail.com>
2020-11-15 20:05:05 -0300

    Eliminar mensaje de inbox borra mensaje en el backend

--- React/src/dominio/mensaje.js
+++ React/src/dominio/mensaje.js
@@ -1,11 +1,12 @@
 export default class Mensaje {
-  constructor(fecha, emisor, leido = false, destinatario, cuerpo) {
+  constructor(fecha, emisor, leido = false, destinatario, cuerpo, id) {
     this.fechaYHoraDeEmision = fecha
     this.fechaYHoraDeLectura = fecha
     this.remitente = emisor
     this.leido = leido
     this.destinatario = destinatario
     this.cuerpo = cuerpo
+    this.id = id
   }
 
   static fromJson(mensajeJSON) {
