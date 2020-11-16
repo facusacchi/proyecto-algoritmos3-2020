f4d28ca996a5568da14177e29feb623c3aeeba87
facusacchi <faq.ndo.n.s@gmail.com>
2020-11-16 11:01:53 -0300

    modifico el menu header para que aparezca en el menu el userLogged

--- React/src/menu-header/menu-header.js
+++ React/src/menu-header/menu-header.js
@@ -7,25 +7,30 @@ export class MenuHeader extends Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            items: [],
+        };
 
-        this.items = [
-            /* {
-                label: this.props.usuario,
-                items: [ */
-            { label: "usuarioService.userLogged.nombreYApellido", icon: 'pi pi-fw pi-user', disabled: true },
+    }
+    
+    componentDidMount() {
+        const items = [
+            { label: usuarioService.userLogged.nombreYApellido, icon: 'pi pi-fw pi-user', disabled: true },
             { separator: true },
             { label: 'Inbox', icon: 'pi pi-fw pi-inbox', url: '/inbox' },
             { label: 'Enviar', icon: 'pi pi-fw pi-arrow-circle-right', url: '/contactos' },
             { label: 'Salir', icon: 'pi pi-fw pi-sign-out', url: '/' }
         ]
-        /*  }
-     ] */
+
+        this.setState({
+            items,
+        })
     }
 
     render() {
         return (
             <div>
-                <Menu model={this.items} popup ref={el => this.menu = el} id="popup_menu" />
+                <Menu model={this.state.items} popup ref={el => this.menu = el} id="popup_menu" />
                 <Button icon="pi pi-bars" onClick={(event) => this.menu.toggle(event)} aria-controls="popup_menu" aria-haspopup />
             </div>
         )
